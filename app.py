fd08fcaff8c48b77c565a11b2398eb5ef5bb701d
xdfg12456 <xdfg12456@gmail.com>
2024-12-09 15:32:10 +0800

    create base

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
-import os
-import json
+from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
 import os
-import json
-from linebot import LineBotApi, WebhookHandler
-from linebot.models import (
-    TextSendMessage, StickerSendMessage, ImageSendMessage, VideoSendMessage,
-    AudioSendMessage, LocationSendMessage, TemplateSendMessage, CarouselTemplate,
-    CarouselColumn, ButtonsTemplate, PostbackAction, URIAction, MessageAction,
-    ImageCarouselTemplate, ImageCarouselColumn, QuickReply, QuickReplyButton,
-    ConfirmTemplate, CameraAction, CameraRollAction, LocationAction,
-    DatetimePickerAction, FlexSendMessage, BubbleContainer, BoxComponent,
-    TextComponent, ImageComponent, ImagemapSendMessage, BaseSize,
-    URIImagemapAction, MessageImagemapAction, ImagemapArea
-) # 看需要用到甚麼再import甚麼
 
-token = 'haqKuA8wK6tb6ufQT7RoL+lpp//xk9w/kCtmgZ3sgng+WHJa67GtnPa/s5TOzcPQETmcMQWKR92/59e9mtVbtbGVO+VGML8nPkERjRxw5gSo+feO2xSB+GOsrXg7Tp8Ypnghkn+5AF51mFUHwYoetQdB04t89/1O/w1cDnyilFU='
-secret = '2e8243533a67cd9fca5a885d1091b244'
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
