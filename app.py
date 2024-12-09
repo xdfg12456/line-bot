7761785675d27de781ee098a44ec0ae3c2b249a8
xdfg12456 <xdfg12456@gmail.com>
2024-12-09 16:36:07 +0800

    re

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    message = TextSendMessage(text=event.reply_token)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
